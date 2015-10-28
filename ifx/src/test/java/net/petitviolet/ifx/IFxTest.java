90f3a38b8b4f1fa6871378f05c811aa8df7ca435
petitviolet <violethero0820@gmail.com>
2015-10-29 00:40:36 +0900

    add get method test

--- ifx/src/test/java/net/petitviolet/ifx/IFxTest.java
+++ ifx/src/test/java/net/petitviolet/ifx/IFxTest.java
@@ -4,15 +4,34 @@ import net.petitviolet.ifx.func.Action;
 
 import org.junit.Test;
 
-import java.lang.Override;
-import java.lang.String;
-
 import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class IFxTest {
+    @Test
+    public void ifxTestFail() throws Exception {
+        String result = IFx.<String>of(false).apply("hoge")
+                .ElseIf(false).apply("foo").get();
+
+        assert result == null;
+    }
+    @Test
+    public void ifxTestGet1() throws Exception {
+        String result = IFx.<String>of(true).apply("hoge")
+                .ElseIf(false).apply("foo").get();
+
+        assert result == "hoge";
+    }
+
+    @Test
+    public void ifxTestGet2() throws Exception {
+        String result = IFx.<String>of(false).apply("hoge")
+                .ElseIf(true).apply("foo").get();
+
+        assert result == "foo";
+    }
     @Test
     public void ifxTest1() throws Exception {
         String result = IFx.<String>of(true).apply("hoge")
